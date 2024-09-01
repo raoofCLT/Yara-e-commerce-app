a90651e26c320aede2764f4477b681ae1c09a2ef
Abdul Raoof <raoofkottayil@gmail.com>
2024-09-01 22:23:24 +0530

    Complete first part of app

---
+++ src/data.js
@@ -0,0 +1,78 @@
+
+export const sliderItems = [
+    {
+        id: 1,
+        img: "/Images/Hero1.png",
+        title: "SUMMER SALE" ,
+        description: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        img: "/Images/Hero2.png",
+        title: "WINTER SALE" ,
+        description: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fcf1ed",
+    },
+    {
+        id: 3,
+        img: "/Images/Hero3.png",
+        title: "POPULAR SALE" ,
+        description: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fbf0f4",
+    },
+]
+
+export const categories = [
+    {
+        id: 1,
+        img: "/Images/Category-1.jpg",
+        title: "LIGHT JACKET",
+    },
+    {
+        id: 2,
+        img: "/Images/Category-2.jpg",
+        title: "SHIRT STYLE",
+    },
+    {
+        id: 3,
+        img: "/Images/Category-3.jpg",
+        title: "LOUNGWEAR LOVE",
+    }
+]
+
+
+export const popularProducts = [
+    {
+        id: 1,
+        img: "/Images/t-shirt.png"
+    },
+    {
+        id: 2,
+        img: "/Images/Shirt.png"
+    },
+    {
+        id: 3,
+        img: "/Images/Top.png"
+    },
+    {
+        id: 4,
+        img: "/Images/Jacket.png"
+    },
+    {
+        id: 5,
+        img: "/Images/GirlDress.png"
+    },
+    {
+        id: 6,
+        img: "/Images/Tshirt.png"
+    },
+    {
+        id: 7,
+        img: "/Images/Cap.png"
+    },
+    {
+        id: 8,
+        img: "/Images/Bag.png"
+    }
+]
