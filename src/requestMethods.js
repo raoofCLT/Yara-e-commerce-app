0a0a28d2d50c2c61b35891fa91bdf9b3b2301055
Abdul Raoof <raoofkottayil@gmail.com>
2024-10-15 23:03:04 +0530

    fix bugs in client

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,16 +1,14 @@
-import axios from "axios"
+import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api/";
+const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NmQ5NzgxNzU0ODgxZTFiYWVhZTc3M2YiLCJpc0FkbWluIjp0cnVlLCJpYXQiOjE3MjYzNzg0OTgsImV4cCI6MTcyNjQ2NDg5OH0.uMPUHKR3MqVkR-SwVBcO_gtuwc3TgjavSRO0u_MMroc";
 
-const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken
 
-export const publicRequest = axios.create({
-    baseURL: BASE_URL,
+export const publicRequest = axios.create ({
+    baseURL:BASE_URL,
 })
 
-export const userRequest = axios.create({
-    baseURL: BASE_URL,
-    header: {token:`Bearer ${TOKEN}`}
+export const userRequest = axios.create ({
+    baseURL:BASE_URL,
+    headers:{token: `Bearer ${TOKEN}`},
 })
-
- 
